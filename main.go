d96ca7c8a14f07da73bf2b1ee6a140564187b4eb
zyuomo <r4nd0mmachine@gmail.com>
2022-03-02 23:32:13 +0100

    Update main.go

--- main.go
+++ main.go
@@ -16,15 +16,11 @@ func env(url string) bool {
 	response, err := http.Get("https://" + url + "/.env")
 
 	if err != nil {
-
 		_, netErrors := http.Get("https://www.google.com")
 
 		if netErrors != nil {
-
 			return false
-
 		}
-
 		return false
 	}
 
@@ -33,29 +29,19 @@ func env(url string) bool {
 	body, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-
 		return false
-
 	}
 
 	if response.StatusCode == 200 {
-
 		x := strings.Contains(string(body), "APP_ENV=")
 
 		if x {
-
 			fmt.Println(".env: " + url)
-
 		} else {
-
 			return false
-
 		}
-
 	}
-
 	return false
-
 }
 
 func awsfunc(url string) bool {
@@ -67,11 +53,8 @@ func awsfunc(url string) bool {
 		_, netErrors := http.Get("https://www.google.com")
 
 		if netErrors != nil {
-
 			return false
-
 		}
-
 		return false
 	}
 
@@ -80,9 +63,7 @@ func awsfunc(url string) bool {
 	body, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-
 		return false
-
 	}
 
 	if response.StatusCode == 200 {
@@ -90,51 +71,35 @@ func awsfunc(url string) bool {
 		other, err2 := regexp.MatchString(`smtp\.sendgrid\.net|smtp\.mailgun\.org|smtp-relay\.sendinblue\.com|smtp.tipimail.com|smtp.sparkpostmail.com|vonage|nexmo|twilo|smtp.deliverabilitymanager.net|smtp.mailendo.com|mail.smtpeter.com|mail.smtp2go.com|smtp.socketlabs.com|secure.emailsrvr.com|mail.infomaniak.com|smtp.pepipost.com|smtp.elasticemail.com|smtp25.elasticemail.com|pro.turbo-smtp.com|smtp-pulse.com|in-v3.mailjet.com`, string(body))
 
 		if akia {
-
 			fmt.Println("[AKIA]: " + url)
-
 		}
 
 		if err != nil {
-
 			log.Fatal(err)
-
 		}
 
 		if other {
-
 			fmt.Println("[OTHER]: " + url)
-
 		}
 
 		if err2 != nil {
-
 			log.Fatal(err2)
-
 		}
-
 		fmt.Println(".aws: " + url)
-
 	}
-
 	return false
 
 }
 
 func phpinfo(url string) bool {
-
 	response, err := http.Get("http://" + url + "/phpinfo.php")
 
 	if err != nil {
-
 		_, netErrors := http.Get("https://www.google.com")
 
 		if netErrors != nil {
-
 			return false
-
 		}
-
 		return false
 	}
 
@@ -143,13 +108,10 @@ func phpinfo(url string) bool {
 	body, err := ioutil.ReadAll(response.Body)
 
 	if err != nil {
-
 		return false
-
 	}
 
 	if response.StatusCode == 200 {
-
 		x := strings.Contains(string(body), "Registered Stream Socket Transports")
 
 		if x {
@@ -158,40 +120,28 @@ func phpinfo(url string) bool {
 			other, err2 := regexp.MatchString(`smtp\.sendgrid\.net|smtp\.mailgun\.org|smtp-relay\.sendinblue\.com|smtp.tipimail.com|smtp.sparkpostmail.com|vonage|nexmo|twilo|smtp.deliverabilitymanager.net|smtp.mailendo.com|mail.smtpeter.com|mail.smtp2go.com|smtp.socketlabs.com|secure.emailsrvr.com|mail.infomaniak.com|smtp.pepipost.com|smtp.elasticemail.com|smtp25.elasticemail.com|pro.turbo-smtp.com|smtp-pulse.com|in-v3.mailjet.com`, string(body))
 
 			if akia {
-
 				fmt.Println("[AKIA]: " + url)
-
 			}
 
 			if err != nil {
-
 				log.Fatal(err)
-
 			}
 
 			if other {
-
 				fmt.Println("[OTHER]: " + url)
-
 			}
 
 			if err2 != nil {
-
 				log.Fatal(err2)
-
 			}
 
 			fmt.Println("phpinfo: " + url)
 
 		} else {
-
 			return false
-
 		}
 	}
-
 	return false
-
 }
 
 func sql(url string) bool {
@@ -200,28 +150,20 @@ func sql(url string) bool {
 	response2, err2 := http.Get("http://" + url + "/.mysql_history")
 
 	if err != nil {
-
 		_, netErrors := http.Get("https://www.google.com")
 
 		if netErrors != nil {
-
 			return false
-
 		}
-
 		return false
 	}
 
 	if err2 != nil {
-
 		_, netErrors := http.Get("https://www.google.com")
 
 		if netErrors != nil {
-
 			return false
-
 		}
-
 		return false
 	}
 
@@ -229,15 +171,11 @@ func sql(url string) bool {
 	defer response2.Body.Close()
 
 	if err != nil {
-
 		return false
-
 	}
 
 	if err2 != nil {
-
 		return false
-
 	}
 
 	if response.StatusCode == 200 {
@@ -262,9 +200,7 @@ func main() {
 	file, err := os.Open(filename)
 
 	if err != nil {
-
 		log.Fatal(err)
-
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -272,9 +208,7 @@ func main() {
 	var lines []string
 
 	for scanner.Scan() {
-
 		lines = append(lines, scanner.Text())
-
 	}
 
 	rep := len(lines)
